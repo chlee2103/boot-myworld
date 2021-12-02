68f33317bc44fbfe33fc0111cca83c9c36f6648b
eroul-ri <askyws@gravityneocyon.com>
2021-12-02 12:24:33 +0900

    프로젝트 생성

---
+++ src/test/java/com/ex/myworld/MyworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.myworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
